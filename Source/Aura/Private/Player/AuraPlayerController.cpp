7393752a4d69ba2e182fc2c4afaf7ced19da51e8
wenlong054231 <wenlongchow@yahoo.com>
2024-01-14 01:53:12 +0800

    Added ABP, PlayerController

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/AuraPlayerController.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* SubSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubSystem>(GetLocalPlayer());
+}
